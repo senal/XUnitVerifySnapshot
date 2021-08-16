6ad0292496ecde67dd008eb7d65609749b3204fc
senal <ranga.fernando@gmail.com>
2021-08-16 21:22:41 +1000

    Add sample test project to begin with

---
+++ Sample/Model/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Model
+{
+    internal class Person
+    {
+        public Guid Id { get; set; }
+        public string GivenNames { get; set; }
+        public string FamilyName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Address Address { get; set; }
+    }
+}
---
+++ Sample/PersonBuilder.cs
@@ -0,0 +1,49 @@
+﻿using Sample.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+    static class PersonBuilder
+    {
+        static List<Person> People = new()
+        {
+            new Person()
+            {
+                Id = new Guid("e7b6b3f8-5db5-4fdc-877a-f0512f67d68a"),
+                GivenNames = "Emmy",
+                FamilyName = "Annachiara",
+                DateOfBirth = new DateTime(2000, 1, 2, 0, 0, 0),
+                Address = new Address
+                {
+                    Street = "30 Greenwood Parkway",
+                    City = "Jordan Springs",
+                    State = "NSW"
+                }
+            }
+            ,
+            new Person()
+            {
+                Id = new Guid("88a56488-115a-4544-81cb-439e821bdb73"),
+                GivenNames = "",
+                FamilyName = "",
+                DateOfBirth = new DateTime(1982, 3, 8, 0, 0, 0),
+                Address = new Address
+                {
+                    Street = "20 High Street",
+                    City =  "Bankstown",
+                    State = "NSW"
+                }
+            }
+        };
+
+        public static Person Find (Guid id)
+        {
+            return People.SingleOrDefault(i => i.Id == id);
+        }
+        
+    }
+}
